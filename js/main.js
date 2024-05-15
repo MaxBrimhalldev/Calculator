472f431b316973647de2a746f977b0db3051dd2e
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-05-15 13:19:28 -0600

    changed ids
    changed ids for added functionality

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ document.querySelector('button').addEventListener('click',buttonRecognition)
 
 function buttonRecognition(){
 	let total = 0
-	let button1 = 1, button2 = 2, button3 = 3, button4 = 4, button5 = 5, button6 = 6, button7 = 7, button8 = 8, button9 = 9, button0 = 0
+if(){
+	
+}
 }
