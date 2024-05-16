b25565819c4c32425fabacfd5f150ca65a00d03f
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-05-15 20:47:19 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-// get value of display
-
-//create button press
+// get value of buttons
+let total = 0
+let display = document.querySelector('#calcDisplay')
+document.querySelector('#one').addEventListener('click',one)
+document.querySelector('#two').addEventListener('click',)
+document.querySelector('#three').addEventListener('click',)
+document.querySelector('#add').addEventListener('click',)
 // Functions for opperand, numbers and clear
+function one(){
+
+}
 //How does the numbers function work?
 //
