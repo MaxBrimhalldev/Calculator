bdcd3b9f34a79ba9b602e6f409a5f69756fde807
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-06-04 16:29:15 -0600

    It sucks

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 // get value of buttons
 let total = 0
 let display = document.querySelector('#calcDisplay')
-document.querySelector('#one').addEventListener('click',one)
-document.querySelector('#two').addEventListener('click',)
-document.querySelector('#three').addEventListener('click',)
-document.querySelector('#add').addEventListener('click',operand)
-document.querySelector('#clear').addEventListener('click',clear)
-// Functions for opperand, numbers and clear
-function one(){
-	display.innerText += 1
-}
-function operand(){
-	
-}
-function clear(){
-	display.innerText = ''
-}
-//How does the numbers function work?
-//
+let keys = document.querySelector('#calcKeys')
