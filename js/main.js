d1469d13f6ada991e058b5a3346ae8ed134f6b2c
MaxBrimhalldev <max.brimhall.dev@gmail.com>
2024-06-12 10:46:07 -0600

    updated calc
    Can now display 1 and +. Need to add other addatives

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-// get value of buttons
-let Calculator = {
-	
-	calculator.total = 0
-	calculator.display = document.querySelector('#calcDisplay')
-	calculator.keys = document.querySelector('#calcKeys').addEventListener('click', calculate)
-	calculator.calculate (){
-		
+// get value of button
+let total = 0
+let num1 = ''
+let num2 = ''
+let display = ''
+let operator = ''
+
+document.querySelector('.calcKeys').addEventListener('click', displayCalc)
+document.querySelector('#one').addEventListener('click', won)
+document.querySelector('#one').addEventListener('click', two)
+document.querySelector('#plus').addEventListener('click', plus)
+
+
+function won(){
+	if(operator === ''){
+		num1 += '1'
+	}else{
+		num2 += '1'
 	}
+}
+function plus (){
+	if(operator === '+'){
+		operator = '+'
+		num1 + num2
+	}
+	
+}
+
+function displayCalc(){
+	display = num1 + operator + num2
+	
+document.querySelector('#calcDisplay').innerText = display
 }
